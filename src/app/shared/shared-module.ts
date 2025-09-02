4ef2c37314fc79cc0d613a8b670875b695084de0
franky franco <frankyfranco456@gmail.com>
2025-09-02 13:23:24 -0500

    creacion del componente card

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -2,15 +2,16 @@ import { Input, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './components/input/input.component';
 import { IonicModule } from '@ionic/angular';
+import { CardComponent } from './components/card/card.component';
 
 
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, CardComponent],
   imports: [
     CommonModule,
     IonicModule,
   ],
-  exports: [InputComponent],
+  exports: [InputComponent, CardComponent],
 })
 export class SharedModule { }
