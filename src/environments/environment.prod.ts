3fe297cb10cf70208fe9fda6976c9e1a26a2a59b
franky franco <frankyfranco456@gmail.com>
2025-09-05 22:10:31 -0500

    variables a produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  newsapi: {
+    api_key: 'dd69e0d70ddb4301b736fc34039f8f49',
+    baseUrl: 'https://newsapi.org',
+  },
+
+  countriespi: {
+    baseUrl: 'https://countriesnow.space/api/v0.1/countries/flag/unicode'
+  }
 };
